1a877a118481e48e30c9c2ad2d8b0b6fb05854dc
Vincent Morin <vincent.morin@telecomnancy.net>
2017-12-05 00:03:25 +0100

    modif exprfeuille

--- src/CompositePattern/ExprFeuille.java
+++ src/CompositePattern/ExprFeuille.java
@@ -3,7 +3,7 @@ package CompositePattern;
 public abstract class ExprFeuille implements NAire{
 	
 	public NAire getChildren(int a){
-		return null;
+		return null; //une feuille n'a pas de fils
 	}
 
 }
